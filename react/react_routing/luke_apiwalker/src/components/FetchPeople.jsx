73cf6a6599e914146e2843b7142ebc01cbd886f5
Nathan Prater <nprater86@gmail.com>
2022-05-09 19:42:19 -0500

    added ninja bonuses

--- react/react_routing/luke_apiwalker/src/components/FetchPeople.jsx
+++ react/react_routing/luke_apiwalker/src/components/FetchPeople.jsx
@@ -1,26 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router';
+import { useHistory } from 'react-router-dom';
 
 const FetchPeople = props => {
     const { id } = useParams();
-    const [data, setData] = useState({});
+    const [personData, setPersonData] = useState({});
+    const [homeworldData, setHomeworldData] = useState({});
+    const [error, setError] = useState(false);
+    const history = useHistory();
 
     useEffect(() => {
         axios.get(`https://swapi.dev/api/people/${id}`)
             .then(res => {
-                console.log(res.data);
-                setData(res.data);
+                setError(false);
+                setPersonData(res.data);
+                axios.get(res.data.homeworld)
+                    .then(res => {
+                        setError(false);
+                        setHomeworldData(res.data);
+                    })
+                    .catch(err=>{
+                        console.log(err);
+                        setError(true);
+                    })
+            })
+            .catch(err=>{
+                console.log(err);
+                setError(true);
             })
     },[id]);
 
+    function handleLink(e){
+        e.preventDefault();
+        let homeWorldUrl = homeworldData.url;
+        let homeWorldID = homeWorldUrl.slice(30,homeWorldUrl.length-1);
+        history.push(`/planets/${homeWorldID}`)
+    }
+
     return (
         <div>
-            <h1 className="mt-5">{ data.name }</h1>
-            <h3 className="mt-5">Height: <span style={{fontWeight: 'normal'}}>{ data.height }</span></h3>
-            <h3 className="mt-3">Mass: <span style={{fontWeight: 'normal'}}>{ data.mass }</span></h3>
-            <h3 className="mt-3">Hair Color: <span style={{fontWeight: 'normal'}}>{ data.hair_color }</span></h3>
-            <h3 className="mt-3">Skin Color: <span style={{fontWeight: 'normal'}}>{ data.skin_color }</span></h3>
+            {
+                error ?
+                <img className="mt-5" src="https://y.yarn.co/069f448f-8b5d-4813-82cf-70dd7db9fcdc_text.gif" alt="These aren't the droids you're looking for." /> :
+                <div>
+                    <h1 className="mt-5">{ personData.name }</h1>
+                    <h3 className="mt-5">Homeworld: <a href="#" onClick={ e => handleLink(e) } style={{fontWeight: 'normal'}}>{ homeworldData.name }</a></h3>
+                    <h3 className="mt-3">Height: <span style={{fontWeight: 'normal'}}>{ personData.height }</span></h3>
+                    <h3 className="mt-3">Mass: <span style={{fontWeight: 'normal'}}>{ personData.mass }</span></h3>
+                    <h3 className="mt-3">Hair Color: <span style={{fontWeight: 'normal'}}>{ personData.hair_color }</span></h3>
+                    <h3 className="mt-3">Skin Color: <span style={{fontWeight: 'normal'}}>{ personData.skin_color }</span></h3>
+                </div>
+            }
         </div>
     );
 }
--- react/react_routing/luke_apiwalker/src/components/FetchPlanet.jsx
+++ react/react_routing/luke_apiwalker/src/components/FetchPlanet.jsx
@@ -5,22 +5,33 @@ import { useParams } from 'react-router';
 const FetchPlanet = props => {
     const { id } = useParams();
     const [data, setData] = useState({});
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         axios.get(`https://swapi.dev/api/planets/${id}`)
             .then(res => {
-                console.log(res.data);
+                setError(false);
                 setData(res.data);
+            })
+            .catch(err=>{
+                console.log(err);
+                setError(true);
             })
     },[id]);
 
     return (
         <div>
-            <h1 className="mt-5">{ data.name }</h1>
-            <h3 className="mt-5">Climate: <span style={{fontWeight: 'normal'}}>{ data.climate }</span></h3>
-            <h3 className="mt-3">Terrain: <span style={{fontWeight: 'normal'}}>{ data.terrain }</span></h3>
-            <h3 className="mt-3">Surface Water: <span style={{fontWeight: 'normal'}}>{ data.surface_water }</span></h3>
-            <h3 className="mt-3">Population: <span style={{fontWeight: 'normal'}}>{ parseInt(data.population).toLocaleString('en-US') }</span></h3>
+            {
+            error ?
+            <img className="mt-5" src="https://y.yarn.co/069f448f-8b5d-4813-82cf-70dd7db9fcdc_text.gif" alt="These aren't the droids you're looking for." /> :
+            <div>
+                <h1 className="mt-5">{ data.name }</h1>
+                <h3 className="mt-5">Climate: <span style={{fontWeight: 'normal'}}>{ data.climate }</span></h3>
+                <h3 className="mt-3">Terrain: <span style={{fontWeight: 'normal'}}>{ data.terrain }</span></h3>
+                <h3 className="mt-3">Surface Water: <span style={{fontWeight: 'normal'}}>{ data.surface_water }</span></h3>
+                <h3 className="mt-3">Population: <span style={{fontWeight: 'normal'}}>{ isNaN(parseInt(data.population)) ? data.population : parseInt(data.population).toLocaleString('en-US') }</span></h3>
+            </div>
+            }
         </div>
     );
 }
